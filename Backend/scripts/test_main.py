9860786bf5cba0e339732f1791193e13aeb65629
Max Trautwein <trautwein.max@gmail.com>
2023-11-13 12:15:19 +0100

    made the API able to handle parallel Request Initial Test Setup pytest

---
+++ Backend/scripts/test_main.py
@@ -0,0 +1,36 @@
+import pytest
+import main
+# Use the following command to start the database
+# docker compose up postgres
+
+@pytest.fixture()
+def app():
+    app = main.create_app(True)
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner("-s")
+
+
+def test_request_example(client):
+    response = client.get("/TEST")
+
+    assert response.status_code == 200
+    assert b'TEST' in response.data
+    #
+
