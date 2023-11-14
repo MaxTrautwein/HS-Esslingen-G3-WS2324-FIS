cdc54ba9ac1d6c03b7adf9dff3d8dc608c9bbd2f
Max Trautwein <trautwein.max@gmail.com>
2023-11-14 18:38:55 +0100

    added Testcase with mocking

--- Backend/scripts/test_main.py
+++ Backend/scripts/test_main.py
@@ -2,8 +2,7 @@ import pytest
 import main
 import Appointment
 from datetime import date, timedelta, time
-
-
+import db
 
 # Use the following command to start the database
 # docker compose up postgres
@@ -87,3 +86,39 @@ def test_Appointment_Abstract_to_json():
     assert abstract_appointment_json["room"] == test_room
     assert abstract_appointment_json["dateSpan"] == test_dateSpan
 
+
+def test_Appointment(mocker):
+    test_id = 1
+    test_name = "My Super Cool Appointment"
+    test_description = "my Description for my Appointment"
+    test_startTime = time(10, 0)
+    test_endTime = time(13, 0)
+    test_lecturer = 1
+    test_room = 2
+    test_dateSpan = 1
+    test_canceled = False
+    test_roomName = "Test Room Name"
+
+    abstract_appointment = Appointment.Appointment_Abstract(test_id, test_name, test_description, test_startTime
+                                                            , test_endTime, test_lecturer, test_room, test_dateSpan)
+
+    test_today = date.today()
+    test_db = db.Database(mock=True)
+
+    # Now we need to patch the Calls to the Database with the pytest-mocking
+    mocker.patch('db.Database.IsAppointmentCanceld').return_value = test_canceled
+    mocker.patch('db.Database.GetRoomName').return_value = test_roomName
+
+
+    appointment = Appointment.Appointment(abstract_appointment,test_today, test_db)
+
+    assert appointment.id == test_id
+    assert appointment.name == test_name
+    assert appointment.description == test_description
+    assert appointment.startTime == test_startTime
+    assert appointment.endTime == test_endTime
+    assert appointment.lecturer == test_lecturer
+    assert appointment.room == test_room
+    assert appointment.roomName == test_roomName
+    assert appointment.canceled == test_canceled
+    assert appointment.date == test_today
